8c520391be23b7b1cba381ded83dad9e2f146ccf
Joaquim Chianca <joaquimchianca@gmail.com>
2023-01-18 02:41:04 -0300

    Day 3

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,17 +1,25 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-import androidx.compose.material.MaterialTheme
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredSize
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.toComposeImageBitmap
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import extensions.loadImageToBitMap
@@ -22,17 +30,67 @@ import java.net.URL
 @Preview
 fun App() {
     val imageURL = "https://m.media-amazon.com/images/M/MV5BNGNhMDIzZTUtNTBlZi00MTRlLWFjM2ItYzViMjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SY1000_CR0,0,686,1000_AL_.jpg"
-    MaterialTheme {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally
+
+    MaterialTheme(
+        colors = darkColors()
+    ) {
+        Surface(
+            elevation = 4.dp
         ) {
-            Text("Pulp Fiction: Tempo de violência (1994)")
-            Image(
-                bitmap = imageURL.loadImageToBitMap(),
-                contentDescription = "Pulp Fiction",
-                modifier = Modifier.requiredSize(250.dp).padding(8.dp)
-            )
-            Text("Nota: 8.9/10")
+            Box(modifier = Modifier.fillMaxSize()){
+                Column(
+                    modifier = Modifier
+                        .width(200.dp)
+                        .padding(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Image(
+                        bitmap = imageURL.loadImageToBitMap(),
+                        contentDescription = "capa do filme",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clip(RoundedCornerShape(4.dp))
+                    )
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(
+                                top = 8.dp,
+                                start = 8.dp,
+                                end = 8.dp
+                            ),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ){
+                        Row(verticalAlignment = Alignment.CenterVertically){
+                            Icon(
+                                Icons.Filled.Star,
+                                contentDescription = "ícone de nota para filme",
+                                modifier = Modifier.height(16.dp),
+                                tint = Color.Yellow
+                            )
+                            Text("8.9",
+                                modifier = Modifier.padding(start = 2.dp),
+                                color = Color(0xffeeeeee),
+                                fontSize = 14.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+                        }
+                        Text(
+                            "1994",
+                            fontSize = 14.sp,
+                            color = Color(0xffeeeeee)
+                        )
+                    }
+                    Text("Pulp Fiction",
+                        modifier = Modifier.padding(
+                            start = 16.dp,
+                            top = 8.dp,
+                            end = 16.dp
+                        ),
+                        fontSize = 12.sp
+                    )
+                }
+            }
         }
     }
 }
